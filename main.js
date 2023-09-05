b6907963231aeb843036b0c8880388a911c4db40
taeyeon <hahaha2662@naver.com>
2023-09-05 12:55:56 +0900

    .

---
+++ main.js
@@ -0,0 +1,41 @@
+document.getElementById('time').innerHTML = new Date().toLocaleDateString();
+
+function pink() {
+  document.body.style.backgroundColor = 'pink';
+}
+
+function lightblue() {
+  document.body.style.backgroundColor = 'lightblue';
+}
+
+function yellow() {
+  document.body.style.backgroundColor = 'yellow';
+}
+
+function reset() {
+  document.body.style.backgroundColor = 'linen';
+}
+
+function showhtml() {
+  document.getElementById('fig').src = 'img/htmlS.jpg' / 왼쪽에이미지;
+  document.getElementById('desc').src = 'img/htmlS.jpg'.innerHTML;
+  ('~~이다');
+}
+
+function showcss() {
+  document.getElementById('fig').src = 'img/css.jpg' / 왼쪽에이미지;
+  document.getElementById('desc').src = 'img/htmlS.jpg'.innerHTML;
+  ('~~이다');
+}
+
+function showjs() {
+  document.getElementById('fig').src = 'img/js.jpg' / 왼쪽에이미지;
+  document.getElementById('desc').src = 'img/htmlS.jpg'.innerHTML;
+  ('~~이다');
+}
+
+function hide() {
+  document.getElementById('fig').src = '';
+  document.getElementById('desc').src = 'img/htmlS.jpg'.innerHTML;
+  ('~~이다');
+}
